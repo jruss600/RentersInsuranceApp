0fcb1285dce01ec2cd725967e393c1d36ec40838
John Russell <jruss600@gmail.com>
2020-07-14 21:40:37 -0600

    cleaning up unused using statements

--- API/Controllers/ItemsController.cs
+++ API/Controllers/ItemsController.cs
@@ -1,13 +1,10 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Application;
 using Domain;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers
 {
--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -11,20 +11,5 @@ namespace Persistence
         }
 
         public DbSet<Item> Items { get; set; }
-
-        // protected override void OnModelCreating(ModelBuilder builder) {
-        //     builder.Entity<Item>()
-        //         .HasData(
-        //             new Item {Name = "TV", Value = 2000, Category = "Electronics"},
-        //             new Item {Name = "Playstation", Value = 400, Category = "Electronics"},
-        //             new Item {Name = "Stereo", Value = 1600, Category = "Electronics"},
-        //             new Item {Name = "Shirts", Value = 1100, Category = "Clothing"},
-        //             new Item {Name = "Jeans", Value = 1100, Category = "Clothing"},
-        //             new Item {Name = "Pots and Pans", Value = 3000, Category = "Kitchen"},
-        //             new Item {Name = "Flatware", Value = 500, Category = "Kitchen"},
-        //             new Item {Name = "Knife Set", Value = 500, Category = "Kitchen"},
-        //             new Item {Name = "Misc", Value = 1000, Category = "Kitchen"}
-        //         );
-        // }
     }
 }
